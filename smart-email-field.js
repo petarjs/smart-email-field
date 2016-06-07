eba63dec4bc7d65e93be3bcd10f66de5a8e3eea6
Petar Slovic <petar.slovic@gmail.com>
2016-06-07 22:57:17 +0200

    add style tag

--- smart-email-field.js
+++ smart-email-field.js
@@ -5,8 +5,47 @@
   var $wrapper;
   var $shadow;
 
-  // https://github.com/mailcheck/mailcheck/wiki/List-of-Popular-Domains
-  var EMAIL_DOMAINS = ['gmail.com', 'yahoo.com', 'ymail.com', 'outlook.com'];
+  var EMAIL_DOMAINS = [
+    /* Default domains included */
+    'gmail.com', 'att.net', 'comcast.net', 'facebook.com', 'gmail.com', 'gmx.com', 'googlemail.com',
+    'google.com', 'hotmail.com', 'hotmail.co.uk', 'mac.com', 'me.com', 'mail.com', 'msn.com',
+    'live.com', 'sbcglobal.net', 'verizon.net', 'yahoo.com', 'yahoo.co.uk', 'aol.com',
+
+    /* Other global domains */
+    'email.com', 'games.com' /* AOL */, 'gmx.net', 'hush.com', 'hushmail.com', 'icloud.com', 'inbox.com',
+    'lavabit.com', 'love.com' /* AOL */, 'outlook.com', 'pobox.com', 'rocketmail.com' /* Yahoo */,
+    'safe-mail.net', 'wow.com' /* AOL */, 'ygm.com' /* AOL */, 'ymail.com' /* Yahoo */, 'zoho.com', 'fastmail.fm',
+    'yandex.com',
+
+    /* United States ISP domains */
+    'bellsouth.net', 'charter.net', 'comcast.net', 'cox.net', 'earthlink.net', 'juno.com',
+
+    /* British ISP domains */
+    'btinternet.com', 'virginmedia.com', 'blueyonder.co.uk', 'freeserve.co.uk', 'live.co.uk',
+    'ntlworld.com', 'o2.co.uk', 'orange.net', 'sky.com', 'talktalk.co.uk', 'tiscali.co.uk',
+    'virgin.net', 'wanadoo.co.uk', 'bt.com',
+
+    /* Domains used in Asia */
+    'sina.com', 'qq.com', 'naver.com', 'hanmail.net', 'daum.net', 'nate.com', 'yahoo.co.jp', 'yahoo.co.kr', 'yahoo.co.id', 'yahoo.co.in', 'yahoo.com.sg', 'yahoo.com.ph',
+
+    /* French ISP domains */
+    'hotmail.fr', 'live.fr', 'laposte.net', 'yahoo.fr', 'wanadoo.fr', 'orange.fr', 'gmx.fr', 'sfr.fr', 'neuf.fr', 'free.fr',
+
+    /* German ISP domains */
+    'gmx.de', 'hotmail.de', 'live.de', 'online.de', 't-online.de' /* T-Mobile */, 'web.de', 'yahoo.de',
+
+    /* Russian ISP domains */
+    'mail.ru', 'rambler.ru', 'yandex.ru', 'ya.ru', 'list.ru',
+
+    /* Belgian ISP domains */
+    'hotmail.be', 'live.be', 'skynet.be', 'voo.be', 'tvcablenet.be', 'telenet.be',
+
+    /* Argentinian ISP domains */
+    'hotmail.com.ar', 'live.com.ar', 'yahoo.com.ar', 'fibertel.com.ar', 'speedy.com.ar', 'arnet.com.ar',
+
+    /* Domains used in Mexico */
+    'hotmail.com', 'yahoo.com.mx', 'live.com.mx', 'yahoo.com', 'hotmail.es', 'live.com', 'hotmail.com.mx', 'prodigy.net.mx', 'msn.com'
+  ];
 
   function init(selector) {
     $el = $(selector);
@@ -29,8 +68,8 @@
 
   function getShadowField(el) {
     var elCss = window.getComputedStyle(el.get(0), '').cssText;
+    addStyle('.sef-shadow {' + elCss + '}');
     var shadow = $('<div>').addClass('sef-shadow');
-    shadow[0].style.cssText = elCss;
     return shadow;
   }
 
@@ -69,7 +108,7 @@
       var text = ev.target.value;
 
       if(isSpace(ev)) {
-        text += ' '
+        text += ' ';
       }
 
       if(isArrowRight(ev)) {
@@ -102,7 +141,22 @@
 
   }
 
+  function addStyle(css) {
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+
+    style.type = 'text/css';
+    if (style.styleSheet){
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    head.appendChild(style);
+  }
+
   window.SmartEmailField = {
     init: init
   };
 })(window, document, $);
+
